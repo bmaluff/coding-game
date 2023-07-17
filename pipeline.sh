add2497dbe920898eb8c1728987d1945c26bd9eb
Bader Maluff <bmaluff@gmail.com>
2023-07-17 16:43:39 -0400

    adding comments to script

--- pipeline.sh
+++ pipeline.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-# ejecutar tests
-# ejecutar coverage tests
-# construir y subir
-# deplegar la app en kubernetes
+echo "Iniciando Minikube"
 minikube start
+echo "Redirigiendo el repositorio de Docker"
 eval $(minikube -p minikube docker-env)
+echo "Construyendo la imagen"
 docker build -t cg-prod-1 -f Dockerfile_production .
 
 cd database
+echo "Iniciando la Base de Datos"
 kubectl apply -f configmap-db.yaml
 kubectl apply -f secret-db.yaml
 kubectl apply -f storage.yaml
+
+echo "Iniciando el Backend"
 kubectl apply -f deployment-db.yaml
 kubectl apply -f service-db.yaml
 
+echo "Iniciando el Frontend"
 cd ../application
 kubectl apply -f configmap.yaml
 kubectl apply -f secret.yaml
@@ -21,6 +24,6 @@ kubectl create configmap nginx-config --from-file=nginx.conf
 kubectl create configmap migrations --from-file=migrations.sh
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
-echo "Waiting on pods to start..."
+echo "Esperando que el pod se levante..."
 sleep 10s
 kubectl port-forward service/app-service 8080:80
